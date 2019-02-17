958d6ecf40e1103a54701df0c8e2afa4f492d926
gusttavo212 <gusttavo212@gmail.com>
2019-02-17 18:58:56 -0300

    Start a compenent

---
+++ src/components/Conversor.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="conversor">
+        <h2>{{moedaA}} Para {{moedaB}}</h2>
+
+    </div>
+</template>
+
+<script>
+export default {//Propriedades do componente que irao ser passadas
+    name: "Conversor",
+    props: ["moedaA","moedaB"]    
+}
+</script>
+
+<style <style lang="scss" scoped>
+
+</style>
+
+
